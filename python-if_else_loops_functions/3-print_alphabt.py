906eb6cbb3bbd791e2711fd09ba7d36eb9dbfc6e
yousrakdc <yousrakdc@gmail.com>
2024-05-06 12:59:04 +0000

    not q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-print(
-    "".join(
-        f"{chr(i)}"
-        for i in range(97, 123)
-        if chr(i) not in ('q', 'e')
-    ),
-    end=''
-)
+alphabet = ''.join(chr(i) for i in range(97, 123) if chr(i) not in ('q', 'e'))
+
+print("{}".format(alphabet), end="")
