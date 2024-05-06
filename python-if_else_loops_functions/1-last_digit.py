132db1d6c4408d04b2b75b00f6463ef2da52aead
yousrakdc <yousrakdc@gmail.com>
2024-05-06 12:00:10 +0000

    last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = number % 10
-if last_digit < 0:
-    last_digit += 10
+last_digit = abs(number) % 10
 
 if last_digit > 5:
     message = "and is greater than 5"
