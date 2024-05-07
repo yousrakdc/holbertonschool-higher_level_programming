c0e7fd1608d857679aa0f581d5e7b4fa6ed66417
yousrakdc <yousrakdc@gmail.com>
2024-05-07 07:55:02 +0000

    uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 def uppercase(str):
-    uppercase_str = ""
-    for char in str:
 
+    uppercase_chars = []
+
+    for char in str:
         if 'a' <= char <= 'z':
             uppercase_char = chr(ord(char) - 32)
         else:
-
             uppercase_char = char
 
-        uppercase_str += uppercase_char
+        uppercase_chars.append(uppercase_char)
 
-    print(uppercase_str)
+    uppercase_str = ''.join(uppercase_chars)
+    print("{}".format(uppercase_str))
 
 
 uppercase("best")
