63cd1010723cee0a04f7e03b3cfc2fbd802a627e
yousrakdc <yousrakdc@gmail.com>
2024-05-07 09:44:13 +0000

    infinite

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    total_sum = 0
+
+    for i in range(1, len(sys.argv)):
+        total_sum += int(sys.argv[i])
+
+    print(total_sum)
+
+
+if __name__ == "__main__":
+    main()
