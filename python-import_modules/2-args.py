6b465bb10a11b88486d58242880290cbd124bc9b
yousrakdc <yousrakdc@gmail.com>
2024-05-07 09:32:18 +0000

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,17 +5,15 @@ import sys
 def main():
     argv = sys.argv
 
-    num_args = len(argv) - 1
-    separator = ':' if num_args > 0 else '.'
-
-    arg_word = "argument" if num_args == 1 else "arguments"
-
-    output = "Number of {}{}{}".format(arg_word, separator, num_args)
-
-    print(output)
+    if num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
 
     if num_args > 0:
+
         for i in range(1, len(argv)):
+
             print("{}: {}".format(i, argv[i]))
 
 
