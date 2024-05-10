75934e89f6e92f96dd517d7d14458157308146f3
yousrakdc <yousrakdc@gmai.com>
2024-05-10 13:31:30 +0000

    problem

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 import hidden_4
 
 if __name__ == "__main__":
-    for content in dir(hidden_4):
-        if content[:2] != "__":
-            print("{:s}".format(content))
+    for hidden in dir(hidden_4):
+        if hidden[0] != "_":
+            print(hidden)
