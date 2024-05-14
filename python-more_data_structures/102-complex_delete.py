86b9d044f30e5f986c4f2bd74b81c7c734f48dbf
yousrakdc <yousrakdc@gmail.com>
2024-05-14 12:54:29 +0000

    advanced

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    dictionary_copy = a_dictionary.copy()
+
+
+for key, val in dictionary_copy.items():
+    val == value:
+        del a_dictionary[key]
