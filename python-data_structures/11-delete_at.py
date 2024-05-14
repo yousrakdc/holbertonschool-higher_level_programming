d42f21142279729a24e047c923993f170d8751e0
yousrakdc <yousrakdc@gmail.com>
2024-05-14 07:49:13 +0000

    delete

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,11 +2,9 @@
 
 def delete_at(my_list=[], idx=0):
 
-    new_list = []
+    if idx < 0 or idx >= len(my_list):
+        return my_list
 
-    for i, item in enumerate(my_list):
-
-        if i != idx:
-            new_list.append(item)
-
+    new_list = my_list[:idx] + my_list[idx+1:]
+    
     return new_list
