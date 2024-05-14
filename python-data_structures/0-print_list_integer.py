8f61d0cc390cf2e9c6637a766edb3a31ff997d06
yousrakdc <yousrakdc@gmail.com>
2024-05-14 06:50:19 +0000

    modified

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for integer in my_list:
-        print("{}".format(integer))
+    for int in my_list:
+        print(str.format('{:d}', int))
