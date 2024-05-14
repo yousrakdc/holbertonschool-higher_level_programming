7192ea5edf93bd8def2c532def55747fdeaea23b
yousrakdc <yousrakdc@gmail.com>
2024-05-14 07:23:28 +0000

    matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print("")
+        return
+
     for row in matrix:
+        row_str = ""
         for num in row:
-            print("{:d}".format(num), end=" ")
-        print()
+            row_str += "{:d} ".format(num)
+        print(row_str.rstrip())
