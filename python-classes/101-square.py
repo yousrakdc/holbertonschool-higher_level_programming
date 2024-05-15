e1f3563c06f6ed9ebb1eb8c5972ce4c20549972f
yousrakdc <yousrakdc@gmail.com>
2024-05-15 17:59:59 +0200

    help

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""square advanced task, pls send help"""
 
-class Square():
+
+class Square:
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
@@ -23,8 +25,8 @@ class Square():
 
     @position.setter
     def position(self, value):
-        if not (isinstance(value, tuple) and len(value) == 2 and 
-                all(isinstance(num, int) for num in value) and 
+        if not (isinstance(value, tuple) and len(value) == 2 and
+                all(isinstance(num, int) for num in value) and
                 all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self._position = value
