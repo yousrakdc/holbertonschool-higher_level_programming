797e1557a2f78becc5b5b84dd96a151a06668a9d
yousrakdc <yousrakdc@gmail.com>
2024-05-15 12:03:38 +0200

    complexe delete

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    new_dict = {}
-
+    keys_to_delete = []
     for key, val in a_dictionary.items():
-         if val != value:
-            new_dict[key] = val
+        if val == value:
+            keys_to_delete.append(key)
+    for key in keys_to_delete:
+        del a_dictionary[key]
 
-    return new_dict
+
+my_dictionary = {'a': 1, 'b': 2, 'c': 3, 'd': 2, 'e': 1}
+complex_delete(my_dictionary, 2)
+print(my_dictionary)
