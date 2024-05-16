d87d91eb78d253019cbe129e300a5326e0559d67
yousrakdc <yousrakdc@gmail.com>
2024-05-16 13:34:31 +0200

    help

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -2,51 +2,52 @@
 """square advanced task, pls send help"""
 
 
-class Square:
+class Square():
+    """Define a square"""
+
     def __init__(self, size=0, position=(0, 0)):
+        """Square initialization"""
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        return self._size
+        """Retrieve size"""
+        return self.__size
 
     @size.setter
     def size(self, value):
+        """Set size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self._size = value
+        self.__size = value
 
     @property
     def position(self):
-        return self._position
+        """Retrieve position"""
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if not (isinstance(value, tuple) and len(value) == 2 and
-                all(isinstance(num, int) for num in value) and
-                all(num >= 0 for num in value)):
+        """Set position"""
+        if not isinstance(value, tuple) or len(value) != 2 or \
+           not all(isinstance(num, int) for num in value) or \
+           not all(num >= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self._position = value
+        self.__position = value
 
     def area(self):
-        return self._size ** 2
+        """Calculate area"""
+        return self.__size ** 2
 
     def my_print(self):
-        if self._size == 0:
-            print()
-            return
-        [print() for _ in range(self._position[1])]
-        for i in range(self._size):
-            print(' ' * self._position[0] + '#' * self._size)
-
-    def __str__(self):
-        result = []
-        if self._size == 0:
-            return "\n"
-        [result.append("") for _ in range(self._position[1])]
-        for i in range(self._size):
-            result.append(' ' * self._position[0] + '#' * self._size)
-        return "\n".join(result)
+        """Print the square"""
+        if self.size == 0:
+            print('')
+        else:
+            for _ in range(self.position[1]):
+                print()
+            for _ in range(self.size):
+                print(' ' * self.position[0] + '#' * self.size)
