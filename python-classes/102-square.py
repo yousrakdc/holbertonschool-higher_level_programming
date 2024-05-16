dc9fb0f59d86b0d61ca749f80ad52bb483a542be
yousrakdc <yousrakdc@gmail.com>
2024-05-16 13:42:49 +0200

    another square

---
+++ python-classes/102-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Class Square that defines a square"""
+
+
+class Square:
+    "Define a private size attribute"
+    def __init__(self, size=0):
+        "Square initialization"
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
