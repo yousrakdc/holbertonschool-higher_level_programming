6430e561a07bab705200b43231b511221ae79832
yousrakdc <yousrakdc@gmail.com>
2024-05-21 09:43:33 +0200

    add integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """Check if a is an integer or a float"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    """Check if b is an integer or a float"""
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    """ Cast both a and b to integers if they are floats"""
+    a = int(a)
+    b = int(b)
+
+    return a + b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
