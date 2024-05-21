00de89a1c5b4d5362af5214636a03de3c4d8a214
yousrakdc <yousrakdc@gmail.com>
2024-05-21 11:45:05 +0200

    say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,6 +2,7 @@
 
 """ Function that prints My name is <first name> <last name>"""
 
+
 def say_my_name(first_name, last_name=""):
     """
     Print the provided first and last name
@@ -21,4 +22,4 @@ def say_my_name(first_name, last_name=""):
     if last_name:
         print("My name is {} {}".format(first_name, last_name))
     else:
-        print("My name is", first_name)
+        print("My name is ", first_name)
