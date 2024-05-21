d27a07cfe8979876e1aa3cca0f8c86f4e21c2198
yousrakdc <yousrakdc@gmail.com>
2024-05-21 11:11:36 +0200

    documented

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+    adds two integers together
 
+    Args:
+        a, b = integers
+        b = 98
+"""
 def add_integer(a, b=98):
     """Check if a is an integer or a float"""
     if not isinstance(a, (int, float)):
@@ -10,7 +16,7 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     """ Cast both a and b to integers if they are floats"""
-    a = int(a)
-    b = int(b)
+    a0 = int(a)
+    b0 = int(b)
 
-    return a + b
+    return a0 + b0
