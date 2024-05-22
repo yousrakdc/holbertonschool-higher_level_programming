2d2684d328fcf8ba17e5501a659811577089ecf3
yousrakdc <yousrakdc@gmail.com>
2024-05-22 14:00:53 +0200

    my list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Write a class MyList that inherits from list"""
+
+
+class MyList:
+    """MyList inherits from list"""
+    def print_sorted(self):
+        """prints list"""
+        print(sorted(self))
