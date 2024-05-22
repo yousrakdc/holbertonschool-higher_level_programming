c8a20d0b78b81c113d36c72c205765cdc39c067d
yousrakdc <yousrakdc@gmail.com>
2024-05-22 17:40:38 +0200

    multiple inheritance

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""The Enigmatic FlyingFish - Exploring Multiple Inheritance"""
+
+
+class Fish:
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+
+class Bird:
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
+
+
+if __name__ == "__main__":
+    """Testing and Method Resolution Order Exploration"""
+    flying_fish = FlyingFish()
+
+    flying_fish.fly()
+    flying_fish.swim()
+    flying_fish.habitat()
+    print(FlyingFish.mro())
