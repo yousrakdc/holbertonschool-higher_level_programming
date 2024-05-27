caf4d737559a852f6b20ad3149bc2b39ec2d5d1a
yousrakdc <yousrakdc@gmail.com>
2024-05-27 14:26:29 +0200

    pascal triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Create a function that returns a list of lists of int
+reprensenting the Pascal's triangle of n"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        """Each row starts with 1"""
+        row = [1]
+        prev_row = triangle[-1]
+
+        for j in range(1, i):
+            """Calculate the values for the current row"""
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
