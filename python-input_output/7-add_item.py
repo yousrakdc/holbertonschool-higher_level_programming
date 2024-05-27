fdb73687995d44c0ad99917be7c0535f4c607619
yousrakdc <yousrakdc@gmail.com>
2024-05-27 13:12:24 +0200

    load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,9 +2,21 @@
 """Script that adds all arguments to a Python list
 and then saves them to a file"""
 
+import json
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+import os
+
+
+def save_to_json_file(my_obj, filename):
+    """Save the JSON string representation of object to file"""
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
+
+
+def load_from_json_file(filename):
+    """Load from JSON"""
+    with open(filename, 'r') as file:
+        return json.load(file)
 
 
 try:
