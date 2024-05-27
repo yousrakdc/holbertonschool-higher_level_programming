a19644f16c330ffdc745da60d9322ecb90101c4b
yousrakdc <yousrakdc@gmail.com>
2024-05-27 13:42:02 +0200

    new

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,17 +3,13 @@
 
 
 class Student:
-    """Initializes a Student instance with first name, last name, and age."""
+    """student class"""
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance."""
+        return self.__dict__
+
     def __init__(self, first_name, last_name, age):
+        """Initializes Student instance with first name, last name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
-
-def to_json(self):
-    """Retrieves a dictionary representation of a Student instance."""
-    return {
-        "first_name": self.first_name,
-        "last_name": self.last_name,
-        "age": self.age
-    }
