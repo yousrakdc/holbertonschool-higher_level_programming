ecf913be577783416ad6e508cbdfe0fba8458891
yousrakdc <yousrakdc@gmail.com>
2024-05-27 15:39:00 +0200

    pascal

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,20 +4,15 @@ reprensenting the Pascal's triangle of n"""
 
 
 def pascal_triangle(n):
-    """returns an empty list"""
-    if n > 0:
-        triangle = [[1]]
-
-    for i in range(1 - n):
-        """Each row starts with 1"""
-        row = [1]
-        if i > 0:
-            n = len(triangle)
-            for j in range(n - 1):
-                """Calculate the values for the current row"""
-                row.append(triangle[i][j] + triangle[j + 1])
-
-            row.append(1)
-            triangle.append(row)
+    triangle = []
+    if n <= 0:
         return triangle
-    return []
+    triangle = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(triangle[i - 1]) - 1):
+            curr = triangle[i - 1]
+            temp.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
+        temp.append(1)
+        triangle.append(temp)
+    return triangle
