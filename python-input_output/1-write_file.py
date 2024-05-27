81af6b32ca286538856e317d18b4ecc4be1529e1
yousrakdc <yousrakdc@gmail.com>
2024-05-27 10:37:01 +0200

    write file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""function that writes a string to a text file (UTF8)
+returns the number of characters written"""
+
+
+def write_file(filename="", text=""):
+    """Writes file"""
+    with open(filename, 'w', encoding="UTF-8") as f:
+        print(f.write(text), end="")
+    return len(text)
