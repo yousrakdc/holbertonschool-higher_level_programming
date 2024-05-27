ea839b2723deabe0aeedf15f904114046b52d521
yousrakdc <yousrakdc@gmail.com>
2024-05-27 13:20:49 +0200

    class to json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,7 +7,7 @@ def class_to_json(obj):
     attributes = {}
 
     for attr_name in dir(obj):
-        if not attr_name.startswith('___'):
+        if not attr_name.startswith('__'):
             attr_value = getattr(obj, attr_name)
             if isinstance(attr_value, (list, dict, str, int, bool)):
                 attributes[attr_name] = attr_value
