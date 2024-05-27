fc7b89d49c0919cb2506e8ee5b82cf503ef8631c
yousrakdc <yousrakdc@gmail.com>
2024-05-27 10:23:58 +0200

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,5 @@
 
 def read_file(filename=""):
     """reads file"""
-    try:
-        with open('my_file_0.txt', 'r', encoding="utf-8") as file:
-            print(file.read())
+    with open('my_file_0.txt', 'r', encoding="utf-8") as file:
+        print(file.read())
