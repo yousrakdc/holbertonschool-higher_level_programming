3784fffa4cc1fca5df107715fd12435f137186e5
yousrakdc <yousrakdc@gmail.com>
2024-05-27 14:42:28 +0200

    checker

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """module with function to save string to file"""
-
 import json
 
 
 def save_to_json_file(my_obj, filename):
     """Save the JSON string representation of object to file"""
-    with open(filename, "w") as file:
-        json.dump(my_obj, file)
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
