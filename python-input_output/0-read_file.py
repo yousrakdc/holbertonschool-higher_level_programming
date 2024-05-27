9560c9834c7458c7287674f9f247835fee6b23da
yousrakdc <yousrakdc@gmail.com>
2024-05-27 10:22:20 +0200

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,5 +3,12 @@
 """function that reads a text file (UTF8) and prints it to stdout"""
 
 def read_file(filename=""):
-    with open('my_file_0.txt', 'r', encoding="utf-8") as file:
-        print(file.read())
+    try:
+        with open('my_file_0.txt', 'r', encoding="utf-8") as file:
+            print(file.read())
+    except FileNotFoundError:
+        print(f"Error: The file 'my_file_0.txt' does not exist.")
+    except IOError:
+        print(f"Error: An error occurred while reading the file 'my_file_0.txt'.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
