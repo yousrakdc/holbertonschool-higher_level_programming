aa3ed5ab22a557f03473c5601ba2fd1fb32ebee1
yousrakdc <yousrakdc@gmail.com>
2024-05-27 13:46:03 +0200

    student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student"""
+
+
+class Student:
+    """student class"""
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age
+            }
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes Student instance with first name, last name, and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
