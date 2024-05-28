35fdeec1f70c7b4a65e3e96934d9fc9529ed842b
yousrakdc <yousrakdc@gmail.com>
2024-05-28 10:33:57 +0200

    comments

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
-
-# Pickling Custom Classes
+"""Pickling Custom Classes"""
 import pickle
 
 
 class CustomObject:
     def __init__(self, name, age, is_student):
+        """initialization"""
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
+        """display"""
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
+        """serialize"""
         try:
             with open('filename', 'wb') as file:
                 pickle.dump(self, file)
@@ -24,6 +26,7 @@ class CustomObject:
 
     @classmethod
     def deserialize(cls, filename):
+        """deserialize"""
         try:
             with open('filename', 'rb') as file:
                 obj = pickle.load(file)
