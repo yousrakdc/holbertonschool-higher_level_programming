d39a2dce20263e776548d10d7d2fdb040e948783
yousrakdc <yousrakdc@gmail.com>
2024-05-28 10:32:38 +0200

    docstring

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+"""Convert CSV data to JSON"""
 import csv
 import json
 
 
 def convert_csv_to_json(csv_filename):
+    """converting"""
     try:
         with open(csv_filename, 'r') as csv_file:
             reader = csv.DictReader(csv.file)
