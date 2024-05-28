4c6b2d2b1a97390244820730597fd03b916c9cfd
yousrakdc <yousrakdc@gmail.com>
2024-05-28 10:35:07 +0200

    changes

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,15 +1,18 @@
-# Serialize and deserialize data
+#!/usr/bin/env python3
+"""Serialize and deserialize data"""
 
 import pickle
 
 
 def serialize_and_save_to_file(data, filename):
+    """serialize and save"""
     with open('filename', 'wb') as file:
         pickle.dump(data, file)
         print(f"Data serialized and saved to {filename}")
 
 
 def load_and_deserialize(filename):
+    """deserialize"""
     with open(filename, 'rb') as file:
         deserialized_data = pickle.load(file)
         print("Deserialized Data:")
