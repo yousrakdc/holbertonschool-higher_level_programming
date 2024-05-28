0f277ca4a2ee153dacf031535bb85ce7bf1e4917
yousrakdc <yousrakdc@gmail.com>
2024-05-28 10:16:43 +0200

    csv to json

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import csv
+import json
+
+
+def convert_csv_to_json(csv_filename):
+    try:
+        with open(csv_filename, 'r') as csv_file:
+            reader = csv.DictReader(csv.file)
+            data = [row for row in reader]
+
+        with open('data.json', 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+
+        return True
+    except FileNotFoundError:
+        print(f"Error: The file '{csv_filename}' was not found.")
+        return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
