03c2a32dc78df7a6b9107c40b1bfaa7e5a7143ca
yousrakdc <yousrakdc@gmail.com>
2024-05-29 09:14:12 +0200

    checker

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -13,5 +13,4 @@ def serialize_and_save_to_file(data, filename):
 def load_and_deserialize(filename):
     """deserialize"""
     with open(filename, 'r') as file:
-        data = json.load(file)
-    return data
+        return json.load(file)
