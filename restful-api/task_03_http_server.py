30dc69363a1508a24ab1dc4649fb9952d20c77f5
yousrakdc <yousrakdc@gmail.com>
2024-05-30 09:54:08 +0200

    http

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -2,7 +2,7 @@
 """Develop a simple API using Python with the `http.server` module"""
 import http.server  # Basic web server capabilities
 import socketserver  # TCP server functionalities
-import orjson  # JSON handling capabilities
+import json  # JSON handling capabilities
 import logging
 
 PORT = 8000
@@ -17,22 +17,22 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             """Root path: Return a simple text message"""
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
-            self.end_headers()
+            self.end_header()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == "/data":
             """'/data' path: Return a JSON object"""
             self.send_response(200)
             self.send_header("Content-type", "application/json")
-            self.end_headers()
+            self.end_header()
             response = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(orjson.dumps(response).encode())
+            self.wfile.write(json.dumps(response).encode())
         elif self.path == "/status":
             """'/status' path: Return a status JSON object"""
             self.send_response(200)
             self.send_header("Content-type", "application/json")
-            self.end_headers()
+            self.end_header()
             response = {"status": "OK"}
-            self.wfile.write(orjson.dumps(response).encode())
+            self.wfile.write(json.dumps(response).encode())
         else:
             """Other paths: Return a 404 Not Found response"""
             self.send_error(404, "Endpoint not found")
