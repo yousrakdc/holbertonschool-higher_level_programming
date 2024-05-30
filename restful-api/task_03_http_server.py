a6765f26e60b548503ab17f9435cf7fde14feb42
yousrakdc <yousrakdc@gmail.com>
2024-05-30 09:59:52 +0200

    http

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -17,20 +17,20 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             """Root path: Return a simple text message"""
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
-            self.end_header()
+            self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == "/data":
             """'/data' path: Return a JSON object"""
             self.send_response(200)
             self.send_header("Content-type", "application/json")
-            self.end_header()
+            self.end_headers()
             response = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(response).encode())
         elif self.path == "/status":
             """'/status' path: Return a status JSON object"""
             self.send_response(200)
             self.send_header("Content-type", "application/json")
-            self.end_header()
+            self.end_headers()
             response = {"status": "OK"}
             self.wfile.write(json.dumps(response).encode())
         else:
