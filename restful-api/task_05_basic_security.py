4d98a677aaae346010e3e011cad9dff7410ad41e
yousrakdc <yousrakdc@gmail.com>
2024-05-31 16:33:26 +0200

    hell

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 '''API Security and Authentication Techniques'''
 from flask import Flask, jsonify, request
 from werkzeug.security import generate_password_hash, check_password_hash
