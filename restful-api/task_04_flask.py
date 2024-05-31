6b590b398eb90e4deb0277a066bec5fc70283af0
yousrakdc <yousrakdc@gmail.com>
2024-05-31 11:56:20 +0200

    ...

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,63 +1,54 @@
 #!/usr/bin/env python3
+# task_04_flask.py
+
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# In-memory data store
-users = {
-    "jane": {
-        "username": "jane",
-        "name": "Jane",
-        "age": 28,
-        "city": "Los Angeles"
-    },
-    "john": {
-        "username": "john",
-        "name": "John",
-        "age": 30,
-        "city": "New York"
-    }
-}
+# In-memory storage for users
+users = {}
 
 
-@app.route('/')
+# Root endpoint
+@app.route("/")
 def home():
-    """Root endpoint that returns a welcome message."""
     return "Welcome to the Flask API!"
 
 
-@app.route('/data', methods=['GET'])
-def get_users():
-    """Endpoint to get the list of all users."""
-    user_list = [user for user in users.keys()]
-    return jsonify(user_list)
+# Endpoint to get all usernames
+@app.route("/data", methods=["GET"])
+def get_usernames():
+    usernames = list(users.keys())
+    return jsonify(usernames)
 
 
-@app.route('/status', methods=['GET'])
-def get_status():
-    """Endpoint to check the API status."""
-    return "OK"
-
-
-@app.route('/users/<username>', methods=['GET'])
+# Endpoint to get user details
+@app.route("/users/<username>", methods=["GET"])
 def get_user(username):
-    """Endpoint to get details of a specific user by username."""
     if username in users:
         return jsonify(users[username])
     else:
         return jsonify({"error": "User not found"}), 404
 
 
-@app.route('/add_user', methods=['POST'])
+# Endpoint to add a new user
+@app.route("/add_user", methods=["POST"])
 def add_user():
-    """Endpoint to add a new user."""
     user_data = request.get_json()
-    username = user_data.get('username')
+    username = user_data.get("username")
+
     if username in users:
         return jsonify({"error": "User already exists"}), 409
+
     users[username] = user_data
-    return jsonify({"message": "User added", "user": user_data})
+    return jsonify({"message": "User added", "user": user_data}), 201
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Endpoint to check status
+@app.route("/status", methods=["GET"])
+def get_status():
+    return "OK"
+
+
+if __name__ == "__main__":
+    app.run()
