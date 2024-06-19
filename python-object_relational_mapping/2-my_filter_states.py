70006267a52fc6a1834d94f8a1f9de72ee0bf5ba
yousrakdc <yousrakdc@gmail.com>
2024-06-19 14:04:31 +0200

    pyco

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -17,9 +17,10 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name = '{}'"
-                   "ORDER BY states.id LIMIT 2".format(sys.argv[4]))
+                   "ORDER BY states.id ASC".format(sys.argv[4]))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+
     cursor.close()
     db.close()
