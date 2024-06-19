53534ff1abf8e73eadb7746cb09bcf4e17d9cae7
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-06-19 11:40:04 +0200

    filter

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """takes in an argument and displays all values in the states table"""
 
+
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
@@ -14,8 +16,9 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
-    cursor.execute(query, (sys.argv[4],))
+    query = ("SELECT * FROM states WHERE name = '{}'"
+             "ORDER BY states.id ASC".format(sys.argv[4]))
+    cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
         print(row)
