f1ae80e0c529c9df49f3d69a19d9cf8a64a2e033
yousrakdc <yousrakdc@gmail.com>
2024-06-19 15:11:02 +0200

    fetch all

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" lists all State objects from the database """
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    
+    Session =sessionmaker(bind=engine)
+    db_session = Session()
+    
+    for state in db_session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+        
+    db_session.close()
