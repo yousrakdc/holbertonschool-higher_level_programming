9dc6b43b1cbfb28896696e0bac52a1cb4c5d1976
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-06-19 11:24:50 +0200

    filter

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values in the states table"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{}'"
+                   "ORDER BY states.id LIMIT 2".format(sys.argv[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
