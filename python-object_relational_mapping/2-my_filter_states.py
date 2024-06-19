6b8851390dd9487a5b5fd6c3704405512e91865c
yousrakdc <yousrakdc@gmail.com>
2024-06-19 14:11:42 +0200

    print 1

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE '{}'"
-                   "ORDER BY states.id LIMIT 2".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name = '{}'"
+                   "ORDER BY states.id LIMIT 1".format(sys.argv[4]))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
