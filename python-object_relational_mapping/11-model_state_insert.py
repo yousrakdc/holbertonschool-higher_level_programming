3f4f4489c2977c9b56ac542c8d0d4bf4abfc8a2f
yousrakdc <yousrakdc@gmail.com>
2024-06-20 13:26:17 +0200

    loulou

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument from database"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    db_session = Session()
+
+    db_session.add(State(name="Louisiana"))
+    db_session.commit()
+    louisiana = db_session.query(State).filter(State.name ==
+                                             "Louisiana").first()
+    
+    print(louisiana.id)
+    db_session.close()
