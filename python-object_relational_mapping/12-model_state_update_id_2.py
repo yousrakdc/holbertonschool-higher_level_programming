ad0be38fe96551afcdbdff065d20577febdbfd76
yousrakdc <yousrakdc@gmail.com>
2024-06-20 14:00:23 +0200

    state id

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -9,7 +9,8 @@ import sys
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3])
+                           , pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     db_session = Session()
