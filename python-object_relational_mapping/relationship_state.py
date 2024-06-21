dd178ef0304c275aac4f4aa966b5dae69d4dc0f9
yousrakdc <yousrakdc@gmail.com>
2024-06-21 15:06:16 +0200

    relationships

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -8,9 +8,9 @@ from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 class State(Base):
-    __tablename__="states"
+    __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete-orphan",
-                          backref="states")
+                          backref="state")
